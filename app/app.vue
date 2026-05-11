c00ae692b0316fad253e84530d13d9c0f67950ce
tuncod <mdha0308+tmpgh@gmail.com>
2026-05-11 18:24:49 +0100

    Update app.vue

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,54 @@
+<script setup lang="ts">
+import '@vidstack/react/player/styles/default/theme.css';
+import '@vidstack/react/player/styles/default/layouts/audio.css';
+import '@vidstack/react/player/styles/default/layouts/video.css';
+
+import 'vidstack/player';
+import 'vidstack/player/layouts/default';
+import 'vidstack/player/ui';
+</script>
+
 <template>
   <div>
     <h1 class="text-4xl text-red-600">Vebo</h1>
+    <media-player
+  .viewType="video"
+  .streamType="on-demand"
+  .logLevel="warn"
+  .crossOrigin="true"
+  .playsInline="true"
+  .title="Sprite Fight"
+  .poster="https://files.vidstack.io/sprite-fight/poster.webp"
+>
+  <media-provider>
+    <media-poster class="vds-poster"></media-poster>
+    <source src="https://files.vidstack.io/sprite-fight/hls/stream.m3u8" />
+    <track
+      src="https://files.vidstack.io/sprite-fight/subs/english.vtt"
+      label="English"
+      language="en-US"
+      kind="subtitles"
+      type="vtt"
+      default
+    />
+    <track
+      src="https://files.vidstack.io/sprite-fight/subs/spanish.vtt"
+      label="Spanish"
+      language="es-ES"
+      kind="subtitles"
+      type="vtt"
+    />
+    <track
+      src="https://files.vidstack.io/sprite-fight/chapters.vtt"
+      language="en-US"
+      kind="chapters"
+      type="vtt"
+      default
+    />
+  </media-provider>
+  <media-video-layout
+    .thumbnails="https://files.vidstack.io/sprite-fight/thumbnails.vtt"
+  ></media-video-layout>
+</media-player>
   </div>
 </template>
