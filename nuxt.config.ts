1e0e3f73db4007f009e3fac6dfa996b749be15d8
tuncod <mdha0308+tmpgh@gmail.com>
2026-06-03 23:38:41 +0100

    update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,26 @@ import tailwindcss from "@tailwindcss/vite"
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: false },
+
+  app: {
+    head: {
+      script: [
+        {
+          src: 'https://unpkg.com/eruda'
+        },
+        {
+          innerHTML: `
+            window.addEventListener('load', function () {
+              if (window.eruda) {
+                window.eruda.init();
+              }
+            });
+          `
+        }
+      ]
+    }
+  },
+
   css: ['./app/assets/css/main.css'],
 
   vite: {
@@ -16,4 +36,11 @@ export default defineNuxtConfig({
     '@pinia/nuxt',
     '@vite-pwa/nuxt',
   ],
+})
+
+
+
+// nuxt.config.ts
+export default defineNuxtConfig({
+  
 })
