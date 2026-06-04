f2662f7ab2e3d2402b9346e17a1d4413792934a1
tuncod <mdha0308+tmpgh@gmail.com>
2026-06-04 16:19:19 +0100

    update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,7 +29,9 @@ export default defineNuxtConfig({
           href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+Arabic:wght@100..900&family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap',
         },
       ],
-    }
+    },
+
+  keepalive: true,
   },
 
   css: ['./app/assets/css/main.css'],
