97dc23f4c2a65616a347debb1163dfb9e0a6f6e4
tuncod <mdha0308+tmpgh@gmail.com>
2026-06-04 13:40:48 +0100

    update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -13,5 +13,33 @@ const { data: movies } = await useFetch('https://api.themoviedb.org/3/trending/m
   <div>
     <h1 class="text-4xl text-red-600 mt-26">Index</h1>
     {{ movies }}
+
+
+    <section class="section">
+      <div class="section-header">
+        <h2>Popular</h2>
+      </div>
+
+      <div class="scroll-row">
+        <div
+          v-for="item in movies.results"
+          class="card"
+          tabindex="0"
+          role="button"
+          aria-label="title"
+        >
+          <img
+            class="card-poster"
+            :src="'https://image.tmdb.org/t/p/w500' + item.poster_path"
+            alt="title"
+            loading="lazy"
+            decoding="async"
+          />
+      </div>
+    </div>
+
+    </div>
+  </section>
+  
   </div>
 </template>
