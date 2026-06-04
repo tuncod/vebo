497ec9c06d6ff4fe37b5ff7f647d8f6224f45fff
tuncod <mdha0308+tmpgh@gmail.com>
2026-06-04 13:23:49 +0100

    update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+
+const movies = await ofetch('https://api.themoviedb.org/3/trending/movie/week', {
+  query: {
+    api_key: '8301a21598f8b45668d5711a814f01f6',
+  }
+})
+
+
+</script>
+
 <template>
   <div>
     <h1 class="text-4xl text-red-600 mt-26">Index</h1>
