39a3e7456bff723a6690c704d072b1003dda22ec
tuncod <mdha0308+tmpgh@gmail.com>
2026-06-11 13:33:49 +0100

    update MediaPoster.vue

---
+++ app/components/media/MediaPoster.vue
@@ -0,0 +1,13 @@
+<script setup>
+const { src } = defineProps(['src'])
+</script>
+<template>
+  <img
+    class="card-poster"
+    :src="'https://image.tmdb.org/t/p/w500' + src"
+     alt="title"
+     loading="lazy"
+    decoding="async"
+  />
+</template>
+
