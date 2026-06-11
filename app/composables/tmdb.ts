5af7dc5a17f51d4d75d8f2ac182c3ec3be8548ba
tuncod <mdha0308+tmpgh@gmail.com>
2026-06-11 13:50:34 +0100

    update tmdb.ts

---
+++ app/composables/tmdb.ts
@@ -0,0 +1,10 @@
+const TMDB_API_BASE = 'api/external' // https://api.themoviedb.org/3
+const TMDB_API_KEY = '8301a21598f8b45668d5711a814f01f6'
+
+const tmdbFetch = ofetch.create({
+  baseURL: TMDB_API_BASE
+})
+
+export const useTmdb = async (url: string, options) => {
+  return await tmdbFetch(url, options)
+}
