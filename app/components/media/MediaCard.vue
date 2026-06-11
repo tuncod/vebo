b8c3191003dde79b767d953b1348a4a472e3d1d6
tuncod <mdha0308+tmpgh@gmail.com>
2026-06-11 13:32:46 +0100

    update MediaCard.vue

---
+++ app/components/media/MediaCard.vue
@@ -0,0 +1,13 @@
+<script setup>
+const { poster } = defineProps(['poster'])
+</script>
+<template>
+  <div
+    class="card"
+    tabindex="0"
+    role="button"
+    aria-label="title"
+  >
+    <MediaPoster :src="poster" />
+  </div>
+</template>
