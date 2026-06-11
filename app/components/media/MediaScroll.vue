69bc6941419b22ff50a6504ca968c65d06e8c0fd
tuncod <mdha0308+tmpgh@gmail.com>
2026-06-11 13:28:03 +0100

    update MediaScroll.vue

---
+++ app/components/media/MediaScroll.vue
@@ -0,0 +1,18 @@
+<script setup>
+const { items, title } = defineProps(['items', 'title'])
+</script>
+<template>
+  <section class="section">
+    <div class="section-header">
+      <h2>{{ title }}</h2>
+    </div>
+
+      <div class="scroll-row">
+        <MediaCard
+          v-for="item in items"
+          :poster="item.poster_path"
+        />
+    </div>
+
+  </section>
+</template>
