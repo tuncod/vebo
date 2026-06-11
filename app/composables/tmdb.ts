a7db82032dba9f0e910427329e09c5d92101f2f8
tuncod <mdha0308+tmpgh@gmail.com>
2026-06-11 14:09:09 +0100

    update tmdb.ts

--- app/composables/tmdb.ts
+++ app/composables/tmdb.ts
@@ -3,10 +3,11 @@ import { ofetch } from 'ofetch'
 const TMDB_API_BASE = 'https://vebo-eta.vercel.app/api/external' // https://api.themoviedb.org/3
 const TMDB_API_KEY = '8301a21598f8b45668d5711a814f01f6'
 
-const tmdbFetch = ofetch.create({
-  baseURL: TMDB_API_BASE
-})
+const tmdbFetch = ofetch.create()
 
 export const useTmdb = async (url: string, options) => {
-  return await tmdbFetch(url, options)
+  return await useFetch(url, {
+    ...options,
+    baseURL: TMDB_API_BASE,
+  })
 }
