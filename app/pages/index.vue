8a43df1355e5ca2b81d7ed1586e2486340f0be97
tuncod <mdha0308+tmpgh@gmail.com>
2026-06-11 14:11:12 +0100

    update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,17 +1,10 @@
 <script setup lang="ts">
-
-const data = await useTmdb('trending/movie/week')
-
-onMounted(() => {
-  console.log(`the component is now mounted.`)
-  setInterval(() => console.log(data), 3000)
-})
+const { data: movies } = await useTmdb('trending/movie/week')
 </script>
-
 <template>
   <div>
     <h1 class="text-4xl text-red-600 mt-26">Index</h1>
 
-    {{ data }}
+    <MediaScroll :items="movies.results" title="Popular — شائع" />
   </div>
 </template>
