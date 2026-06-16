779df03da6dd9d8eeec6046ad0af9ddca2441de6
tuncod <mdha0308+tmpgh@gmail.com>
2026-06-16 20:02:36 +0100

    update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -70,15 +70,21 @@ export default defineNuxtConfig({
       name: 'Vebo',
       short_name: 'Vebo',
       description: 'A Nuxt 4 Progressive Web App',
-      theme_color: '#FF3300',
+      theme_color: '#FF0033',
+      display: 'fullscreen',
       icons: [
         {
-          src: '/pwa-icon-192x192.png',
+          src: '/icons/icon-192x192.png',
           sizes: '192x192',
           type: 'image/png',
         },
         {
-          src: '/pwa-icon-512x512.png',
+          src: '/icons/icon-384x384.png',
+          sizes: '384x384',
+          type: 'image/png',
+        },
+        {
+          src: '/icons/icon-512x512.png',
           sizes: '512x512',
           type: 'image/png',
         },
