45e997079d1a09d9336fa99c7ccd6c4cf0f19241
tuncod <mdha0308+tmpgh@gmail.com>
2026-06-17 15:21:38 +0100

    update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,7 +4,7 @@ const { data: movies } = await useTmdb('trending/movie/week')
 <template>
   <div>
     <h1 class="text-4xl text-red-600 mt-26">{{ $t('index') }}</h1>
-{{ movies }}
+
     <MediaScroll :items="[]" title="Popular — شائع" />
   </div>
 </template>
