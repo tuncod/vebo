216c287ec29b060d99d5d0b380136c51ca9d53c8
tuncod <mdha0308+tmpgh@gmail.com>
2026-06-17 20:36:46 +0100

    update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: false },
 
+  routeRules: {
+    '/': { prerender: true },
+  },
+
   app: {
     head: {
       script: [
