4fd3969bfd2499f5cf55f87ec99621789ce6e27c
tuncod <mdha0308+tmpgh@gmail.com>
2026-06-17 13:01:17 +0100

    update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -110,7 +110,37 @@ export default defineNuxtConfig({
       includeAllowlist: true,
     },
     workbox: {
-      globPatterns: ['**/*']
+      globPatterns: ['**/*'],
+    runtimeCaching: [
+      {
+        urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+        handler: 'CacheFirst',
+        options: {
+          cacheName: 'google-fonts-cache',
+          expiration: {
+            maxEntries: 10,
+            maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+          },
+          cacheableResponse: {
+            statuses: [0, 200]
+          }
+        }
+      },
+      {
+        urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
+        handler: 'CacheFirst',
+        options: {
+          cacheName: 'gstatic-fonts-cache',
+          expiration: {
+            maxEntries: 10,
+            maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+          },
+          cacheableResponse: {
+            statuses: [0, 200]
+          },
+        }
+      }
+    ],
     },
   },
 })
