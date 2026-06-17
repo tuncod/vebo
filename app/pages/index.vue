df984f4f8598d9105d35294d74ed68de54a68697
tuncod <mdha0308+tmpgh@gmail.com>
2026-06-17 15:28:37 +0100

    Add index.vue for trending movies display

---
+++ app/pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const { data: movies } = await useTmdb('trending/movie/week')
+</script>
+<template>
+  <div>
+    <h1 class="text-4xl text-red-600 mt-26">{{ $t('index') }}</h1>
+
+    <MediaScroll :items="[]" title="Popular — شائع" />
+  </div>
+</template>
