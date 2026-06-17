21775a90119d063c778c205f2f803d557bc99dc1
Tuncode <mdha0308+tmpgh@gmail.com>
2026-06-17 19:30:15 +0000

    djjd

--- app/stores/timezone.ts
+++ app/stores/timezone.ts
@@ -1,5 +1,9 @@
+import { skipHydrate } from 'pinia'
+
 export const useTimezoneStore = defineStore('timezone', () => {
-  const timezone = ref('Europe/Madrid')
+  const timezone = skipHydrate(
+    ref('Europe/Madrid')
+  )
 
   const setTimezone = (value: string) => {
     timezone.value = value
