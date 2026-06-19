91f2eb17a247c9f4db67ba5e5ba8dab1af7d4959
tuncod <mdha0308+tmpgh@gmail.com>
2026-06-19 20:30:27 +0100

    update MediaScroll.vue

--- app/components/ui/media/MediaScroll.vue
+++ app/components/ui/media/MediaScroll.vue
@@ -2,9 +2,9 @@
 const { items, title } = defineProps(['items', 'title'])
 </script>
 <template>
-  <section class="section w-full px-3">
+  <section class="section w-full">
     <div class="section-header w-full">
-      <h3 class="text-xl mt-10 mb-2">{{ title }}</h3>
+      <h3 class="text-xl my-2">{{ title }}</h3>
     </div>
 
     <div class="scroll-row flex w-[calc(100%_+_var(--spacing) * -3)] overflow-x-auto gap-2 -mx-3 px-3" id="selection" style="scroll-snap-type: x mandatory;">
