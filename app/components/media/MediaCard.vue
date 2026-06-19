4080c8c85764e947a585e52ed6091ef8381dd270
tuncod <mdha0308+tmpgh@gmail.com>
2026-06-19 13:50:43 +0100

    update MediaCard.vue

--- app/components/media/MediaCard.vue
+++ app/components/media/MediaCard.vue
@@ -2,7 +2,7 @@
 const { poster } = defineProps(['poster'])
 </script>
 <template>
-        <div class="card relative w-[130px] w-full h-auto shrink-0 hover:scale-90 active:scale-95 transition">
+        <div class="card relative w-[130px] h-auto shrink-0 hover:scale-90 active:scale-95 transition">
           <div class="relative aspect-[2/3] overflow-hidden rounded-xl bg-slate-100 dark:bg-zinc-800">
             <MediaPoster :src="poster" />
             <div class="absolute py-1 px-2 left-0 right-0 bottom-0 hidden" style="background: linear-gradient(to top, rgba(0,0,0,0.85) 25%, transparent 100%);">
