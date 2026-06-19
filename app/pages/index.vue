a27daa6c7f437ad172bc2eab627b9c0bd9a0b080
tuncod <mdha0308+tmpgh@gmail.com>
2026-06-19 14:07:16 +0100

    update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -10,8 +10,8 @@ onMounted(() => {
 </script>
 <template>
   <div>
-    <h1 class="text-4xl text-red-600 mt-26">{{ $t('index') }}</h1>
-{{ movies }}
+    <h1 class="text-4xl text-red-600 mt-26" @click="refresh">{{ $t('index') }}</h1>
+
     <MediaScroll :items="movies.results" title="Popular — شائع" />
   </div>
 </template>
