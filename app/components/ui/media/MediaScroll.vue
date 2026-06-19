0927cc66420e4a0cb5d0242c6fa310b9dca2a347
tuncod <mdha0308+tmpgh@gmail.com>
2026-06-19 20:41:39 +0100

    update MediaScroll.vue

--- app/components/ui/media/MediaScroll.vue
+++ app/components/ui/media/MediaScroll.vue
@@ -8,7 +8,7 @@ const { items, title } = defineProps(['items', 'title'])
     </div>
 
     <div class="scroll-row flex w-[calc(100%_+_var(--spacing) * -3)] overflow-x-auto gap-2 -mx-3 px-3" id="selection" style="scroll-snap-type: x mandatory;">
-      <UiMediaCard v-for="item in items" :poster="item.poster_path" :title="item.original_title" />
+      <UiMediaCard v-for="item in items" :poster="item.poster_path" :title="item.original_title" :score="item.vote_average" />
     </div>
   </section>
 </template>
