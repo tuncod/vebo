434c446244041d19109e08b93fca17ba880d9992
tuncod <mdha0308+tmpgh@gmail.com>
2026-06-19 14:03:11 +0100

    update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-const movies = await useTMDB().trending.movies({ time_window: 'week' })
+const { data: movies, error, pending, refresh, clear } = await useAsyncData('mountains', () => useTMDB().trending.movies({ time_window: 'week' }))
 
 onMounted(() => {
   const layout = window.localStorage.getItem('app_layout')
@@ -11,7 +11,7 @@ onMounted(() => {
 <template>
   <div>
     <h1 class="text-4xl text-red-600 mt-26">{{ $t('index') }}</h1>
-
+{{ movies }}
     <MediaScroll :items="movies.results" title="Popular — شائع" />
   </div>
 </template>
