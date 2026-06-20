658969dc11ea3c0c3fd27e0d7f7df77590eecac7
tuncod <mdha0308+tmpgh@gmail.com>
2026-06-20 15:41:32 +0100

    update MediaCard.vue

--- app/components/ui/media/MediaCard.vue
+++ app/components/ui/media/MediaCard.vue
@@ -5,7 +5,7 @@ const score = computed(() => Number(props.score).toFixed())
 const year = computed(() => props.year.split("-")[0])
 </script>
 <template>
-        <div class="card relative w-[130px] h-auto shrink-0 hover:scale-90 active:scale-95 transition">
+        <NuxtLink to="/" class="card relative w-[130px] h-auto shrink-0 hover:scale-90 active:scale-95 transition">
           <div class="relative aspect-[2/3] overflow-hidden rounded-xl bg-slate-100 dark:bg-zinc-800">
             <UiMediaPoster :src="props.poster" />
             <div class="absolute py-1 px-2 left-0 right-0 bottom-0" style="background: linear-gradient(to top, rgba(0,0,0,0.85) 25%, transparent 100%);">
@@ -17,5 +17,5 @@ const year = computed(() => props.year.split("-")[0])
               <div class="text-zinc-800 dark:text-zinc-300 text-sm text-nowrap">{{ props.title }}</div>
               <div class="text-zinc-600 dark:text-zinc-500 text-xs mb-2">2026 • Drama</div>
             </div>
-        </div>
+        </NuxtLink>
 </template>
