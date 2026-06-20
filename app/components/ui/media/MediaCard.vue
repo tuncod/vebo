75765d3487e0e07ffbab3c58282ca852b8ddfcba
tuncod <mdha0308+tmpgh@gmail.com>
2026-06-20 19:43:12 +0100

    update MediaCard.vue

--- app/components/ui/media/MediaCard.vue
+++ app/components/ui/media/MediaCard.vue
@@ -29,7 +29,7 @@ const year = computed(() => props.year.split("-")[0])
         <NuxtLink :to="props.link" class="card relative w-[130px] h-auto shrink-0 hover:scale-90 active:scale-95 transition">
           <div class="relative aspect-[2/3] overflow-hidden rounded-xl bg-slate-100 dark:bg-zinc-800">
             <UiMediaPoster :src="props.poster" />
-            <div class="absolute py-1 px-2 left-0 right-0 bottom-0" style="background: linear-gradient(to top, rgba(0,0,0,0.85) 25%, transparent 100%);">
+            <div class="absolute py-1 px-2 left-0 right-0 bottom-0 bg-gradient-to-t from-black/85 from-25% to-transparent">
               <div v-if="props.title" class="text-white text-sm text-shadow-md">{{ props.title }}</div>
               <div class="text-zinc-300 text-xs mb-2 text-shadow-md [&>*:not(:last-child)]:after:content-['•'] [&>*:not(:last-child)]:after:mx-1.5">
                 <span v-if="props.year">{{ year }}</span>
