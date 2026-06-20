131b6e6b7424c4353b55017ea3a03d80e344c979
tuncod <mdha0308+tmpgh@gmail.com>
2026-06-20 19:55:43 +0100

    update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
 const { data: movies, error, pending, refresh, clear } = await useAsyncData('trending_movies_week', () => useTMDB().trending.movies({ time_window: 'week' }))
 
+const fullscreen = () => {
+  document.documentElement.requestFullscreen()
+}
+
 onMounted(() => {
   const layout = window.localStorage.getItem('app_layout')
   if (layout) {
@@ -9,8 +13,10 @@ onMounted(() => {
 })
 </script>
 <template>
-  <h1 class="text-5xl font-black my-8 ml-6">Home</h1>
-  <div class="py-5 px-3">
+  <div class="px-3">
+    <h1 class="text-5xl font-black my-8" @click="fullscreen">Home</h1>
+  </div>
+  <div class="px-3">
     <UiMediaScroll :items="movies.results" title="Popular">
       <template #item="{ poster_path, original_title, vote_average, release_date }">
         <UiMediaCard :poster="poster_path" :title="original_title" :score="vote_average" :year="release_date" />
