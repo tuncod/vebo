8ee5520f68838d10f3283ca3ab2bb608f2d6d9d0
tuncod <mdha0308+tmpgh@gmail.com>
2026-06-20 18:24:16 +0100

    update MediaScroll.vue

--- app/components/ui/media/MediaScroll.vue
+++ app/components/ui/media/MediaScroll.vue
@@ -1,14 +1,25 @@
-<script setup>
-const { items, title } = defineProps(['items', 'title'])
+<script setup lang="ts">
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: false,
+  },
+  items: {
+    type: Array,
+    required: true,
+  },
+})
 </script>
 <template>
   <section class="section w-full">
-    <div class="section-header w-full">
-      <h3 class="text-xl my-2">{{ title }}</h3>
+    <div v-if="props.title" class="section-header w-full">
+      <h3 class="text-xl my-2">{{ props.title }}</h3>
     </div>
 
     <div class="scroll-row flex w-[calc(100%_+_var(--spacing) * -3)] overflow-x-auto gap-2 my-3 -mx-3 px-3" id="selection" style="scroll-snap-type: x mandatory;">
-      <UiMediaCard v-for="item in items" :poster="item.poster_path" :title="item.original_title" :score="item.vote_average" :year="item.release_date" />
+      <!-- <UiMediaCard v-for="item in items" :poster="item.poster_path" :title="item.original_title" :score="item.vote_average" :year="item.release_date" /> -->
+      <slot />
     </div>
   </section>
 </template>
