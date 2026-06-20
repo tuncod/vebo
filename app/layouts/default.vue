a219acf7cb9b0a27cc2318847738becbd1c4a853
tuncod <mdha0308+tmpgh@gmail.com>
2026-06-20 15:45:49 +0100

    update default.vue

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+  document.addEventListener('contextmenu', (e) => e.preventDefault())
+</script>
+
 <template>
   <main class="w-full min-h-screen bg-white dark:bg-[#111] select-none" role="main" aria-live="polite">
     <slot />
