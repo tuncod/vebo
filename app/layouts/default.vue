ae012c6b5cea85b6679bfebfba63da2b790448d4
tuncod <mdha0308+tmpgh@gmail.com>
2026-06-20 15:42:14 +0100

    update default.vue

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="w-full min-h-screen bg-white dark:bg-[#111]" role="main" aria-live="polite">
+  <main class="w-full min-h-screen bg-white dark:bg-[#111] select-none" role="main" aria-live="polite">
     <slot />
   </main>
 </template>
