bb89e4ffa6dcef6f35c6c63e08d9414ef10994d1
tuncod <mdha0308+tmpgh@gmail.com>
2026-06-21 12:56:41 +0100

    update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-const { data: movies, error, pending, refresh, clear } = await useAsyncData('trending_movies_week', () => useTMDB().trending.movies({ time_window: 'week' }))
+const { data: movies } = await useAsyncData('trending_movies_week', () => useTMDB().trending.movies({ time_window: 'week' }))
+const { data: shows } = await useAsyncData('trending_shows_week', () => useTMDB().trending.tv({ time_window: 'week' }))
 
 const fullscreen = () => {
   document.documentElement.requestFullscreen()
@@ -17,9 +18,16 @@ onMounted(() => {
     <h1 class="text-5xl font-black my-8" @click="fullscreen">{{ $t('pages.home') }}</h1>
   </div>
   <div class="px-3">
-    <UiMediaScroll :items="movies.results" title="Popular">
+    <UiMediaScroll :items="movies.results" title="This Week's Trending Movies">
       <template #item="{ poster_path, original_title, vote_average, release_date }">
         <UiMediaCard :poster="poster_path" :title="original_title" :score="vote_average" :year="release_date" />
+      </template>
+    </UiMediaScroll>
+  </div>
+  <div class="px-3">
+    <UiMediaScroll :items="shows.results" title="Popular">
+      <template #item="{ name }">
+        {{ name }}
       </template>
     </UiMediaScroll>
   </div>
