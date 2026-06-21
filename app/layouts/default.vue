069246dbcab19fa5821aa845e2bc755062fc7497
tuncod <mdha0308+tmpgh@gmail.com>
2026-06-21 23:41:40 +0100

    update default.vue

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -11,6 +11,34 @@ useHead({
   }
 })
 
+const navbar = [
+  {
+    key: 'home',
+    icon: 'home',
+    path: '/',
+  },
+  {
+    key: 'discover',
+    icon: 'earth',
+    path: '/discover',
+  },
+  {
+    key: 'watchlist',
+    icon: 'heart',
+    path: '/watchlist',
+  },
+  {
+    key: 'settings',
+    icon: 'settings',
+    path: '/settings',
+  },
+  {
+    key: 'auth',
+    icon: 'user',
+    path: '/auth',
+  }
+]
+
 onMounted(() => {
   document.addEventListener('contextmenu', (e) => e.preventDefault())
 })
@@ -18,11 +46,15 @@ onMounted(() => {
 
 <template>
   <NuxtLoadingIndicator />
-  <div class="font-body w-full min-h-screen">
-    <main class="w-full min-h-screen bg-white dark:bg-[#111] select-none" role="main" aria-live="polite">
+  <div class="font-body w-full min-h-screen bg-white dark:bg-[#111] select-none">
+    <main class="w-full min-h-screen" role="main" aria-live="polite">
       <slot />
     </main>
     <div class="w-full h-16"></div>
-    <div class="w-full h-16 fixed bottom-0 left-0 right-0 bg-white/40 backdrop-filter backdrop-blur-lg border-t border-zinc-200/40 z-20"></div>
+    <div class="flex justify-between items-center w-full h-16 fixed bottom-0 left-0 right-0 bg-white/40 backdrop-filter backdrop-blur-lg border-t border-zinc-200/40 z-20 lg:hidden">
+      <NuxtLink v-for="item in kdkdk" :to"item.path" class="">
+        <span>{{ $t(item.key) }}</span>
+      </NuxtLink>
+    </div>
   </div>
 </template>
