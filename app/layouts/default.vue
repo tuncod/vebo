75fddfe54c3e3835afb4b24c008cd8d9e3e47bbb
tuncod <mdha0308+tmpgh@gmail.com>
2026-06-21 22:44:54 +0100

    update default.vue

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -18,7 +18,11 @@ onMounted(() => {
 
 <template>
   <NuxtLoadingIndicator />
-  <main class="font-body w-full min-h-screen bg-white dark:bg-[#111] select-none" role="main" aria-live="polite">
-    <slot />
-  </main>
+  <div class="font-body w-full min-h-screen">
+    <main class="w-full min-h-screen bg-white dark:bg-[#111] select-none" role="main" aria-live="polite">
+      <slot />
+    </main>
+    <div class="w-full h-16"></div>
+    <div class="w-full h-16 fixed bottom-0 left-0 right-0 bg-white/40 backdrop-filter backdrop-blur-lg border-t border-zinc-200/40 z-20"></div>
+  </div>
 </template>
