ae1fa4942227642f47d1ae797ffd8f9412a0d836
tuncod <mdha0308+tmpgh@gmail.com>
2026-06-22 12:55:58 +0100

    update default.vue

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -52,7 +52,7 @@ onMounted(() => {
     </main>
     <div class="w-full h-16"></div>
     <div class="flex justify-between items-center w-full h-16 fixed bottom-0 left-0 right-0 bg-white/40 backdrop-filter backdrop-blur-lg border-t border-zinc-200/40 z-20 lg:hidden">
-      <NuxtLink v-for="item in kdkdk" :to"item.path" class="">
+      <NuxtLink v-for="item in navbar" :to"item.path" class="">
         <span>{{ $t(item.key) }}</span>
       </NuxtLink>
     </div>
