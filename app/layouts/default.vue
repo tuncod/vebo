f926ecbfee3498a7f770cc410e3526591340a92e
tuncod <mdha0308+tmpgh@gmail.com>
2026-06-22 16:41:53 +0100

    update default.vue

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -57,7 +57,7 @@ onMounted(() => {
       class="flex justify-between items-center w-full h-16 fixed bottom-0 left-0 right-0 bg-white/40 backdrop-filter backdrop-blur-lg border-t border-zinc-200/40 z-20 lg:hidden"
     >
       <NuxtLink class="" v-for="item in navbar" :to="item.path">
-        <span><component :is="item.icon" size="28px" /></span>
+        <span><component :is="item.icon" class="size-4" /></span>
       </NuxtLink>
     </div>
   </div>
