88d7306b3b10c723cd876e800119c6a2e028a086
tuncod <mdha0308+tmpgh@gmail.com>
2026-06-23 02:39:08 +0100

    update tmdb.ts

--- app/plugins/tmdb.ts
+++ app/plugins/tmdb.ts
@@ -14,8 +14,8 @@ export default defineNuxtPlugin(() => {
   return {
     provide: {
       tmdb: () => {
-        const store = useTimezoneStore()
-        const { locale } = useI18n()
+        // const store = useTimezoneStore()
+        // const { locale } = useI18n()
 
         return new TMDB('demo-key', {
           // language: locale.value ?? 'en-US',
