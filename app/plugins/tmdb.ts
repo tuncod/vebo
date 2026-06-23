748a7648405a18f1109126f9e3d8166350d3491a
tuncod <mdha0308+tmpgh@gmail.com>
2026-06-23 02:31:59 +0100

    update tmdb.ts

--- app/plugins/tmdb.ts
+++ app/plugins/tmdb.ts
@@ -1,3 +1,15 @@
+import { TMDB as BaseTMDB } from '@lorenzopant/tmdb'
+import type { TMDBOptions } from '@lorenzopant/tmdb/types'
+
+const TMDB_API_BASE = 'https://vebo-eta.vercel.app/api/external' // https://api.themoviedb.org/3
+
+class TMDB extends BaseTMDB {
+  constructor(accessToken: string, options: TMDBOptions = {}) {
+    super(accessToken, options)
+    this.client.baseUrl = TMDB_API_BASE
+  }
+}
+
 export default defineNuxtPlugin(() => {
   return {
     provide: {
