66760565222a9f07835f5c4f40b414ebc51258f0
tuncod <mdha0308+tmpgh@gmail.com>
2026-06-23 14:56:41 +0100

    update settings.vue

--- app/pages/settings.vue
+++ app/pages/settings.vue
@@ -19,27 +19,27 @@ const isDark = computed({
 <template>
   <div class="mt-6 px-3 flex-col">
     <h1 class="text-4xl text-red-600 mt-26">settings</h1>
-    <label>Language</label>
-    <select v-model="language">
-      <option v-for="item in locales" :key="item.code" :value="item.code">
-        {{ item.name }}
-      </option>
-    </select>
-    <div class="flex items-center gap-3">
-    <span>☀️</span>
 
-    <button
-      class="relative h-7 w-14 rounded-full transition-colors"
-      :class="isDark ? 'bg-blue-600' : 'bg-gray-300'"
-      @click="isDark = !isDark"
-    >
-      <div
-        class="absolute top-1 h-5 w-5 rounded-full bg-white transition-transform"
-        :class="isDark ? 'translate-x-8' : 'translate-x-1'"
-      />
-    </button>
+    <div class="max-w-sm mx-auto">
+      <label for="languages" class="block mb-2.5 text-sm font-medium text-heading">Select an option</label>
+      <select v-model="language" id="languages" class="block w-full px-3 py-2.5 bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand shadow-xs placeholder:text-body">
+        <option selected>Choose a language</option>
+        <option v-for="item in locales" :key="item.code" :value="item.code">
+            {{ item.name }}
+        </option>
+      </select>
+    </div>
 
-    <span>🌙</span>
-  </div>
+    <div class="max-w-sm mx-auto">
+      <label class="w-80 inline-flex cursor-pointer p-4 bg-neutral-primary-soft border border-default rounded-base shadow-xs">
+        <input type="checkbox" v-model="isDark" class="sr-only peer">
+        <div class="shrink-0 relative w-9 h-5 bg-neutral-quaternary peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-brand-soft dark:peer-focus:ring-brand-soft rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-buffer after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-brand"></div>
+        <div class="ms-2.5 select-none">
+          <p class="text-sm font-medium text-heading mb-1">Weekly newsletter</p>
+          <p class="text-sm font-normal text-body">Save my credentials for easier sign-in in the future.</p>
+        </div>
+      </label>
+    </div>
+
   </div> 
 </template>
