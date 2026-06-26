6c34949127aa1b31f144d4f2b0879e8de58dd53b
tuncod <mdha0308+tmpgh@gmail.com>
2026-06-26 19:21:14 +0100

    update MediaCard.vue

---
+++ app/components/MediaCard.vue
@@ -0,0 +1,16 @@
+<script setup>
+const props = defineProps({
+  id: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  {{ id }}
+</template>
