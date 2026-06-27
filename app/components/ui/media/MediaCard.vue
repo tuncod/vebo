141594dd0b63b96abeedf4b8fee31064f65abde3
tuncod <mdha0308@gmail.com>
2026-06-27 12:13:42 +0000

    jdjd

--- app/components/ui/media/MediaCard.vue
+++ app/components/ui/media/MediaCard.vue
@@ -1,36 +1,27 @@
-<script setup>
-const props = defineProps({
-  link: {
-    type: String,
-    required: false,
-  },
-  title: {
-    type: String,
-    required: false,
-  },
-  poster: {
-    type: String,
-    required: true,
-  },
-  score: {
-    type: String | Number,
-    required: false,
-  },
-  year: {
-    type: String | Number,
-    required: false,
-  },
-  loading: {
-    type: Boolean,
-    required: false,
-  },
+<script setup lang="ts">
+interface Props {
+  link?: string
+  title?: string
+  poster: string
+  score?: string | number
+  year?: string | number
+  loading?: boolean
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  score: 0,
+  year: '1900',
+  loading: false,
 })
 
 const score = computed(() => Number(props.score).toFixed())
-const year = computed(() => props.year.split('-')[0])
+
+const year = computed(() => String(props.year).split('-')[0])
 </script>
+
 <template>
-  <NuxtLink class="card relative w-[130px] h-auto shrink-0 hover:scale-90 active:scale-95 transition" :to="props.link">
+  <div class="aspect-[2/3] overflow-hidden rounded-xl shadow-lg bg-slate-100 dark:bg-zinc-800" v-if="loading"></div>
+  <NuxtLink class="card relative w-[130px] h-auto shrink-0 hover:scale-90 active:scale-95 transition" v-else :to="props.link">
     <div class="relative aspect-[2/3] overflow-hidden rounded-xl shadow-lg bg-slate-100 dark:bg-zinc-800">
       <UiMediaPoster :src="props.poster" />
       <div class="absolute py-1 px-2 left-0 right-0 bottom-0 bg-gradient-to-t from-black/85 from-25% to-transparent">
