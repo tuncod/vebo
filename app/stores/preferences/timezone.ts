3ce6b13051b90a32f57ccf30a44689524e58879f
tuncod <mdha0308+tmpgh@gmail.com>
2026-07-10 20:34:57 +0100

    update timezone.ts

--- app/stores/preferences/timezone.ts
+++ app/stores/preferences/timezone.ts
@@ -2,7 +2,7 @@ import { defineStore, skipHydrate } from 'pinia'
 import { useCookie } from '@vueuse/core'
 
 export const useTimezoneStore = defineStore('preferences.timezone', () => {
-  const timezone = skipHydrate(useCookie('preferences.timezone', 'Africa/Tunis'))
+  const timezone = skipHydrate(useLocalStorage('preferences.timezone', 'Africa/Tunis'))
 
   const setTimezone = (value: string) => {
     timezone.value = value
