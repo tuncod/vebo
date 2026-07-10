1d78d71089d8dca7d023aca3fb0afba653c54d34
tuncod <mdha0308+tmpgh@gmail.com>
2026-07-10 20:09:18 +0100

    update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -109,6 +109,7 @@ export default defineNuxtConfig({
   colorMode: {
     preference: 'light', // default value of $colorMode.preference
     fallback: 'light', // fallback value if not system preference found
+    storageKey: 'preferences.theme',
   },
 
   i18n: {
