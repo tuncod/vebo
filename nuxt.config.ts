7dd63635e954fd16407bbc4586ac5a4220c427dc
tuncod <mdha0308+tmpgh@gmail.com>
2026-07-10 20:06:59 +0100

    update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -123,7 +123,7 @@ export default defineNuxtConfig({
   },
 
   pinia: {
-    storesDirs: ['./stores/**', './preferences/**'],
+    storesDirs: ['./stores/**', './stores/references/**'],
   },
 
   ui: {
