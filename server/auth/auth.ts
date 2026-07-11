c43a2dc0432c59bef37e246f1b24565411a2ff85
tuncod <mdha0308+tmpgh@gmail.com>
2026-07-11 02:38:26 +0100

    update auth.ts

--- server/auth/auth.ts
+++ server/auth/auth.ts
@@ -1,6 +1,6 @@
 import { betterAuth } from 'better-auth'
 import { drizzleAdapter } from '@better-auth/drizzle-adapter'
-import { db } from '~/server/db'
+import { db } from '~~/server/db'
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
