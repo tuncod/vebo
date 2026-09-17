f925c65ca9434e5761981df57604a4e2ea873dc6
tuncod <mdha0308+tmpgh@gmail.com>
2026-09-17 10:53:27 +0100

    Change i18n strategy to 'no_prefix'

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -115,7 +115,7 @@ export default defineNuxtConfig({
   i18n: {
     locale: 'ar-TN',
     defaultLocale: 'ar-TN',
-    strategy: 'prefix_and_default', // 'no_prefix',
+    strategy: 'no_prefix', // 'prefix_and_default', // 'no_prefix',
     detectBrowserLanguage: {
       cookieKey: 'preferences.language',
     },
